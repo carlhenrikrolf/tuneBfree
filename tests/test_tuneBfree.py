40f1bd0e33e5c1685acc07494e8e704f8c4d3793
Naren Ratan <narenr@fastmail.com>
2024-03-02 13:42:53 +0000

    Run tests in CI

--- tests/test_tuneBfree.py
+++ tests/test_tuneBfree.py
@@ -6,6 +6,7 @@ https://github.com/narenratan/mtsespy
 """
 import subprocess
 from pathlib import Path
+from sys import platform
 
 import pytest
 
@@ -14,11 +15,38 @@ from mtsespy import scala_files_to_frequencies
 
 REPO_DIR = Path(__file__).parents[1]
 
-SCL_FILES = [
+SCL_FILES = sorted(
     str(x) for x in (REPO_DIR / "tests/scala_scale_archive/scl/").glob("*.scl")
-]
+)
 
 mts.reinitialize()
+
+
+def test_scala_files_found():
+    """
+    Check that the scale archive files have been found.
+
+    Makes sure tests fail if scale archive submodule hasn't been cloned.
+    """
+    assert SCL_FILES
+
+
+def test_lib_mts_found():
+    """
+    Check that the MTS-ESP shared library is installed.
+
+    This is to make sure the Scala tunings are picked up; if the library is not
+    installed the default 12TET tuning is used.
+    """
+    if platform == "linux":
+        lib_path = Path("/usr/local/lib/libMTS.so")
+    elif platform == "darwin":
+        lib_path = Path("/Library/Application Support/MTS-ESP/libMTS.dylib")
+    elif platform == "win32":
+        lib_path = Path("/c/Program Files/Common Files/MTS-ESP/LIBMTS.dll")
+    else:
+        raise ValueError("Unexpected platform", platform)
+    assert lib_path.exists()
 
 
 @pytest.mark.parametrize("scl_file", SCL_FILES)
