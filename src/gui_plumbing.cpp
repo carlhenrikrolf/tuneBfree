eb3407f381a142a602e572f2d5bd34f532852690
Naren Ratan <narenr@fastmail.com>
2024-05-11 00:30:17 +0100

    clang-format

--- src/gui_plumbing.cpp
+++ src/gui_plumbing.cpp
@@ -83,7 +83,8 @@ static void GUICreate(MyPlugin *plugin)
     SetWindowLongPtr(plugin->gui->window, 0, (LONG_PTR)plugin);
 
     // PluginPaint(plugin, plugin->gui->bits);
-    plugin->gui->win = new ce::window("Launchpad tuner", 0, ce::rect{0, 0, GUI_WIDTH, GUI_HEIGHT}, plugin->gui->window);
+    plugin->gui->win = new ce::window("Launchpad tuner", 0, ce::rect{0, 0, GUI_WIDTH, GUI_HEIGHT},
+                                      plugin->gui->window);
     plugin->gui->win->on_close = []() {};
     plugin->gui->view = new ce::view(*plugin->gui->win);
 }
@@ -151,7 +152,8 @@ static void GUICreate(MyPlugin *plugin)
                      ButtonReleaseMask | KeyPressMask | KeyReleaseMask | StructureNotifyMask |
                      EnterWindowMask | LeaveWindowMask | ButtonMotionMask | KeymapStateMask |
                      FocusChangeMask | PropertyChangeMask);
-    plugin->gui->win = new ce::window("Launchpad tuner", 0, ce::rect{0, 0, GUI_WIDTH, GUI_HEIGHT}, plugin->gui->window);
+    plugin->gui->win = new ce::window("Launchpad tuner", 0, ce::rect{0, 0, GUI_WIDTH, GUI_HEIGHT},
+                                      plugin->gui->window);
     plugin->gui->win->on_close = []() {};
     plugin->gui->view = new ce::view(*plugin->gui->win);
 }
@@ -228,7 +230,8 @@ static void GUIOnPOSIXFD(MyPlugin *plugin)
 #elif defined(__APPLE__)
 #define GUI_API CLAP_WINDOW_API_COCOA
 
-struct GUI {
+struct GUI
+{
     ce::window *win;
     ce::view *view;
 };
@@ -236,22 +239,29 @@ struct GUI {
 extern "C" void MacSetParent(void *_mainView, void *_parentView);
 extern "C" void MacSetVisible(void *_mainView, bool show);
 
-
-static void GUICreate(MyPlugin *plugin) {
+static void GUICreate(MyPlugin *plugin)
+{
     assert(!plugin->gui);
-    plugin->gui = (GUI *) calloc(1, sizeof(GUI));
+    plugin->gui = (GUI *)calloc(1, sizeof(GUI));
     plugin->gui->win = new ce::window("tuneBfree", 0, ce::rect{0, 0, GUI_WIDTH, GUI_HEIGHT});
     plugin->gui->win->on_close = []() {};
     plugin->gui->view = new ce::view(*plugin->gui->win);
 }
 
-static void GUIDestroy(MyPlugin *plugin) {
+static void GUIDestroy(MyPlugin *plugin)
+{
     assert(plugin->gui);
     free(plugin->gui);
     plugin->gui = nullptr;
 }
 
-static void GUISetParent(MyPlugin *plugin, const clap_window_t *parent) { MacSetParent(plugin->gui->view->host(), parent->cocoa); }
-static void GUISetVisible(MyPlugin *plugin, bool visible) { MacSetVisible(plugin->gui->view->host(), visible); }
+static void GUISetParent(MyPlugin *plugin, const clap_window_t *parent)
+{
+    MacSetParent(plugin->gui->view->host(), parent->cocoa);
+}
+static void GUISetVisible(MyPlugin *plugin, bool visible)
+{
+    MacSetVisible(plugin->gui->view->host(), visible);
+}
 static void GUIOnPOSIXFD(MyPlugin *) {}
 #endif
